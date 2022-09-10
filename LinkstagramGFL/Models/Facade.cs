568cef128111cb6584cef78fcdb819c74858a634
Denis chuma <chumiks1999@gmail.com>
2022-09-10 18:07:06 +0300

    FacadeDetails

--- LinkstagramGFL/Models/Facade.cs
+++ LinkstagramGFL/Models/Facade.cs
@@ -1,7 +1,9 @@
-﻿namespace LinkstagramGFL.Models
+﻿using LinkstagramGFL.Areas.Identity.Data;
+
+namespace LinkstagramGFL.Models
 {
     public class Facade
     {
-
+        public LinkstagramGFLUser _linkstagramGFL { get; set; }
     }
 }
